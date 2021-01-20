7585cd5573f3ad2043f51706287c2aba372d6e40
雷琪 <l503976309@gmail.com>
2021-01-20 17:55:56 +0800

    ✨feat(ch1 ch2): init
    💠 init project.
    💠 ch1 use maven java project.
    💠 ch2 use default code java project.
    
    (￣_,￣ )

---
+++ ch2/GuessGame/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ ch2/GuessGame/src/app/Player.java
@@ -0,0 +1,14 @@
+package app;
+
+/**
+ * Player
+ */
+public class Player {
+
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I'm guessing " + number);
+    }
+}
