70add2b883fdfd42a3a2d5fce66228b864ab7d25
雷琪 <l503976309@gmail.com>
2021-02-05 22:22:14 +0800

    ✨feat(ch18): rmi
    💠 远程部署的RMI。
    💠 实例，通用服务浏览器。
    
    －O－

---
+++ ch18/ServiceServerBrowser/ServiceServer.java
@@ -0,0 +1,8 @@
+import java.rmi.RemoteException;
+
+import java.rmi.*;
+
+public interface ServiceServer extends Remote {
+    Object[] getServiceList() throws RemoteException;
+    Service getService(Object serviceKey) throws RemoteException;
+}
